dd83872ed02aaab79df0f88493a32ac68a53095f
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2025-05-01 09:00:07 +0000

    made adder of variable length

--- src/adder.v
+++ src/adder.v
@@ -1,4 +1,4 @@
-module adder (input [31:0] a, b,
-              output [31:0] y);
+module adder #(parameter WIDTH=32) (input [WIDTH - 1:0] a, b,
+              output [WIDTH - 1:0] y);
   assign y = a + b;
 endmodule
