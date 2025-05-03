94b46bcea8dcdeba550e1d64ffa9285f6f097c03
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2025-05-03 19:49:01 +0000

    made rd ouptut of wire type

--- src/data_memory.v
+++ src/data_memory.v
@@ -1,6 +1,6 @@
 module dmem(input clk, we,
             input  [31:0] a, wd,
-            output reg [31:0] rd);
+            output [31:0] rd);
 
   reg [31:0] RAM [63:0];
   assign rd = RAM[a[31:2]];  // word aligned
