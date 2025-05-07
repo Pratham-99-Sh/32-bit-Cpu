ecce378ec459b5d1639eb7a9ff183731a5698511
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2025-05-07 08:08:39 +0000

    Hex file generator from assembly Program file using assembler.py

---
+++ assembler/hexGenerator.py
@@ -0,0 +1,11 @@
+# run_assembler.py
+from assemblerNew import assemble, save_hex
+
+with open("/workspaces/32-bit-Cpu/assembler/program.s", "r") as f:
+    source_code = f.read()
+print(source_code)
+
+machine_code = assemble(source_code)
+print(machine_code)
+save_hex(machine_code, "/workspaces/32-bit-Cpu/assembler/program.mem")
+print("Assembled successfully into program.mem")
