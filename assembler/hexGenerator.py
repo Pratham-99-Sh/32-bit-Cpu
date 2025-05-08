d979a7b03318a849ec28f49ba247e7fbd1b0eb8f
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2025-05-08 07:08:09 +0000

    Changed from hardcoded filename to input by user

--- assembler/hexGenerator.py
+++ assembler/hexGenerator.py
@@ -1,7 +1,9 @@
 # run_assembler.py
 from assembler import assemble, save_hex
 
-with open("/workspaces/32-bit-Cpu/assembler/program.s", "r") as f:
+File_Name = input("Enter the name of assembly code file (assemblyCodes/<?>): ")
+
+with open(f"/workspaces/32-bit-Cpu/assembler/assemblyCodes/{File_Name}", "r") as f:
     source_code = f.read()
 print(source_code)
 
