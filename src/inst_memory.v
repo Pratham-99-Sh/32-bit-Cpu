be93af16b780648c7d30dcf76dbe1b55ed5b9ecc
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2025-05-13 10:49:09 +0000

    Removed unnecessary comments

--- src/inst_memory.v
+++ src/inst_memory.v
@@ -3,7 +3,5 @@ module imem(input [31:0] a,
   reg [31:0] RAM[63:0];
   initial
     $readmemh("/workspaces/32-bit-Cpu/assembler/program.mem", RAM);
-    //$readmemh("/workspaces/32-bit-Cpu/src/memfile.dat", RAM, 0, 22);  //since we know our code is of 23 instructions to avoid warning that we get from executing line below commented
-    //$readmemh("/workspaces/32-bit-Cpu/src/memfile.dat", RAM); // here since our file doesn't fill all 64 positions and ends way before it we see a warning.
   assign rd = RAM[a[31:2]]; // word aligned
 endmodule
